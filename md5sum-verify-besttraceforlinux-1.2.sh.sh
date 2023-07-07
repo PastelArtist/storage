6ab4e720780bb6341bddf9cfa30de5b8f6040f38
Filtration <aocial@gmail.com>
2023-07-07 13:53:22 +0800

    Add files via upload

---
+++ md5sum-verify-besttraceforlinux-1.2.sh.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#if [ "$(md5sum /home/path/file1.txt |awk '{print $1)')" != "$(md5sum /home/path/file2.txt |awk '{print $1}')" ]; then
+#echo "Files are the same"
+#else
+#echo "Files are different"
+#fi
+
+$1=05081f46a8ed1e4d32502b76ed78d2c8
+
+checksum=$(md5sum besttrace4linux.zip)
+
+$2=checksum
+
+if [ "$1" = "$2" ]
+	then
+		echo "Pass,MD5 is $checksum"
+	else
+		echo "Fail,Original MD5 is $1,Now $checksum"
+fi
